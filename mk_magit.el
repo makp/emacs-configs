f7027ae5b43589bb091b9a94837e936544dfbb04
Makmiller <makmiller@gmail.com>
2013-12-08 21:24:53 -0500

    Updated my magit-repo-dirs

--- mk_magit.el
+++ mk_magit.el
@@ -2,18 +2,16 @@
 (autoload 'magit-status "magit" nil t)
 (global-set-key (kbd "C-x g") 'magit-status)
 
-(add-hook 'magit-mode-hook
-	  (lambda ()
-	    (define-key magit-status-mode-map (kbd "o") 'helm-ls-git-ls)))
+;; (add-hook 'magit-mode-hook
+;; 	  (lambda ()
+;; 	    (define-key magit-status-mode-map (kbd "o") 'helm-ls-git-ls)))
 ;;; "o" was git submodule
 
 (add-hook 'magit-log-edit-mode-hook
 	  (lambda ()
 	    (orgstruct-mode)))
 
-(setq magit-repo-dirs '("~/Documents/mydocs/notes/prob-and-stats/probability-and-statistics"
-			"~/config-files/general/emacs-config"
-			"~/Documents/mydocs/papers/in-progress/reviews-and-comments/comment_fagan-chapter"
+(setq magit-repo-dirs '("~/config-files/general/emacs-config"
 			"~/Documents/mydocs/papers/in-progress/manuscripts/how-bacteria-socialize"))
 ;;; With one prefix argument, magit will provide magit-repo-dirs for
 ;;; you to complete. If you want to create a new repository, use two
