e9f7e0607e08ae22ecf39fd9426d4a2b0fe99a93
Makmiller <makmiller@gmail.com>
2013-12-17 03:27:24 -0500

    updated magit-repo-dirs; uncommented code for helm-ls-git-ls

--- mk_magit.el
+++ mk_magit.el
@@ -2,9 +2,9 @@
 (autoload 'magit-status "magit" nil t)
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; (add-hook 'magit-mode-hook
-;; 	  (lambda ()
-;; 	    (define-key magit-status-mode-map (kbd "o") 'helm-ls-git-ls)))
+(add-hook 'magit-mode-hook
+	  (lambda ()
+	    (define-key magit-status-mode-map (kbd "o") 'helm-ls-git-ls)))
 ;;; "o" was git submodule
 
 (add-hook 'magit-log-edit-mode-hook
@@ -13,8 +13,9 @@
 
 (setq magit-repo-dirs '("~/elisp/agenda"
 			"~/config-files/general/emacs-config"
-			"~/Documents/mydocs/papers/in-progress/manuscripts/how-bacteria-socialize"
+			"~/Documents/mydocs/papers/in-progress/manuscripts"
 			"~/Documents/mydocs/teaching"))
+
 ;;; With one prefix argument, magit will provide magit-repo-dirs for
 ;;; you to complete. If you want to create a new repository, use two
 ;;; prefix-args.
