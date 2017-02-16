ebf0ab42fcc332ad84fd08d217740f758f4db655
Makmiller <makmiller@gmail.com>
2017-02-16 11:59:28 -0500

    autocomplete config: cleaning, added fuzzy complet; binded ac-expand

--- mk_autocomplete-setup.el
+++ mk_autocomplete-setup.el
@@ -15,16 +15,26 @@
 (setq ac-auto-show-menu t)
 (setq ac-auto-start 2) ; number inserted characters before autocompletion is triggered
 
-
 ;; ===========
 ;; Keybindings
 ;; ===========
-(define-key ac-mode-map (kbd "M-/") 'ac-quick-help)
+;; (define-key ac-mode-map (kbd "M-/") 'ac-quick-help)
 
 (define-key ac-mode-map (kbd "<tab>") 'auto-complete)
-(define-key ac-completing-map "\t" nil) 
+;; for when auto-complete-mode does not start automatically (e.g., you
+;; want completion without inserting any character).
+;; "(kbd "<tab>")" is not supposed to bind C-i but only the TAB key
 
+;; Autocomplete has two ways of completing after completion is
+;; started: ac-expand and ac-complete. ac-complete is set to C-m or
+;; RET.
+
+(define-key ac-completing-map (kbd "TAB") 'ac-expand)
+
+(define-key ac-completing-map "\t" nil) 
 (define-key ac-completing-map (kbd "M-s") 'ac-isearch)
+
+
 
 ;; select candidates with C-n/C-p -- instead of M-n/M-p.
 ;; (setq ac-use-menu-map t)
@@ -35,16 +45,9 @@
 ;; Sources
 ;; =======
 
-;; ;; --------
-;; ;; ac-slime
-;; ;; --------
-;; (add-to-list 'load-path "~/.emacs.d/ac-slime-0.2/")
-;; (require 'ac-slime)
-;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
-;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-;; (eval-after-load "auto-complete"
-;;   '(add-to-list 'ac-modes 'slime-repl-mode))
-
+;; --------
+;; ac-slime
+;; --------
 
 ;; -------
 ;; ac-math
@@ -63,65 +66,11 @@
 ;; ------
 ;; ac-ess
 ;; ------
-(setq ess-use-auto-complete t)
+;; (setq ess-use-auto-complete t)
 
-;; ;; ---------
-;; ;; ac-eshell
-;; ;; ---------
-;; (defun ac-pcomplete ()
-;;   ;; eshell uses `insert-and-inherit' to insert a \t if no completion
-;;   ;; can be found, but this must not happen as auto-complete source
-;;   (flet ((insert-and-inherit (&rest args)))
-;;     ;; this code is stolen from `pcomplete' in pcomplete.el
-;;     (let* (tramp-mode ;; do not automatically complete remote stuff
-;;            (pcomplete-stub)
-;;            (pcomplete-show-list t) ;; inhibit patterns like * being deleted
-;;            pcomplete-seen pcomplete-norm-func
-;;            pcomplete-args pcomplete-last pcomplete-index
-;;            (pcomplete-autolist pcomplete-autolist)
-;;            (pcomplete-suffix-list pcomplete-suffix-list)
-;;            (candidates (pcomplete-completions))
-;;            (beg (pcomplete-begin))
-;;            ;; note, buffer text and completion argument may be
-;;            ;; different because the buffer text may bet transformed
-;;            ;; before being completed (e.g. variables like $HOME may be
-;;            ;; expanded)
-;;            (buftext (buffer-substring beg (point)))
-;;            (arg (nth pcomplete-index pcomplete-args)))
-;;       ;; we auto-complete only if the stub is non-empty and matches
-;;       ;; the end of the buffer text
-;;       (when (and (not (zerop (length pcomplete-stub)))
-;;                  (or (string= pcomplete-stub ; Emacs 23
-;;                               (substring buftext
-;;                                          (max 0
-;;                                               (- (length buftext)
-;;                                                  (length pcomplete-stub)))))
-;;                      (string= pcomplete-stub ; Emacs 24
-;;                               (substring arg
-;;                                          (max 0
-;;                                               (- (length arg)
-;;                                                  (length pcomplete-stub)))))))
-;;         ;; Collect all possible completions for the stub. Note that
-;;         ;; `candidates` may be a function, that's why we use
-;;         ;; `all-completions`.
-;;         (let* ((cnds (all-completions pcomplete-stub candidates))
-;;                (bnds (completion-boundaries pcomplete-stub
-;;                                             candidates
-;;                                             nil
-;;                                             ""))
-;;                (skip (- (length pcomplete-stub) (car bnds))))
-;;           ;; We replace the stub at the beginning of each candidate by
-;;           ;; the real buffer content.
-;;           (mapcar #'(lambda (cand) (concat buftext (substring cand skip)))
-;;                   cnds))))))
-
-;; (defvar ac-source-pcomplete
-;;   '((candidates . ac-pcomplete)))
-
-;; (add-hook 'eshell-mode-hook #'(lambda () 
-;; 				(setq ac-sources '(ac-source-pcomplete))))
-
-;; (add-to-list 'ac-modes 'eshell-mode)
+;; ---------
+;; ac-eshell
+;; ---------
 
 ;; --------
 ;; org-mode
