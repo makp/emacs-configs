33964beb1bfd7e5c40139052de93de6284248e50
Makmiller <makmiller@gmail.com>
2017-02-18 14:09:05 -0500

    uncessary keybinding since downcase-dwin is already binded

--- mk_windows-setup.el
+++ mk_windows-setup.el
@@ -75,8 +75,6 @@
 (global-set-key (kbd "C-x l") 'sticky-window-delete-other-windows)
 (global-set-key (kbd "C-x C-l") 'nil)
 (global-set-key (kbd "C-x C-l") 'delete-other-windows)
-(global-set-key (kbd "C-x C-S-l") 'downcase-region)
-
 
 ;;;###autoload
 (defun sticky-window-keep-window-visible ()
