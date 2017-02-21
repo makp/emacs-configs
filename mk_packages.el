451c814044440844a72a11ae25419a2c25baeae7
Makmiller <makmiller@gmail.com>
2017-02-21 13:17:56 -0500

    install certain packages if missing

--- mk_packages.el
+++ mk_packages.el
@@ -11,7 +11,7 @@
    (or (package-installed-p package)
        (if (y-or-n-p (format "Package %s is missing. Install it? " package)) 
            (package-install package))))
- '(auctex magit helm helm-ls-git fuzzy ace-jump-mode auto-complete autopair gnuplot-mode yasnippet undo-tree multiple-cursors keyfreq highlight-parentheses region-bindings-mode dropdown-list zenburn-theme ac-math popup emms w3m ess key-chord ibuffer-git web-mode yaml-mode))
+ '(auctex magit helm helm-ls-git helm-swoop helm-bibtex fuzzy ace-jump-mode ace-isearch auto-complete autopair gnuplot-mode yasnippet undo-tree multiple-cursors keyfreq highlight-parentheses region-bindings-mode dropdown-list zenburn-theme ac-math popup emms w3m ess key-chord ibuffer-git web-mode yaml-mode))
 
 (provide 'mk_packages)
 ;;; mk_packages ends here
