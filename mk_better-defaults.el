a50b0a02d5d941ab5a17f2a4901388ceb36aa4e4
Makmiller <makmiller@gmail.com>
2017-09-22 08:23:40 -0400

    give aggressive-indent-mode a try

--- mk_better-defaults.el
+++ mk_better-defaults.el
@@ -398,6 +398,12 @@
 ;; Indentation
 ;; ===========
 
+;; -----------------
+;; aggressive-indent
+;; -----------------
+(global-aggressive-indent-mode 1)
+;; (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+
 ;; =========================
 ;; Enabling some keybindings
 ;; =========================
