23f60b7fe5a627e42cdd403dda567f28226d232e
Makmiller <makmiller@gmail.com>
2017-10-04 22:32:01 -0400

    updated list of used packages

--- mk_packages.el
+++ mk_packages.el
@@ -11,7 +11,42 @@
    (or (package-installed-p package)
        (if (y-or-n-p (format "Package %s is missing. Install it? " package)) 
            (package-install package))))
- '(auctex magit helm helm-ls-git helm-swoop helm-bibtex fuzzy ace-jump-mode ace-isearch auto-complete autopair gnuplot-mode yasnippet undo-tree multiple-cursors keyfreq highlight-parentheses region-bindings-mode dropdown-list zenburn-theme ac-math popup emms w3m ess key-chord ibuffer-git web-mode yaml-mode))
+ '(
+   ac-math
+   ace-isearch
+   ace-jump-mode
+   aggressive-indent
+   auctex 
+   auto-complete
+   autopair
+   dired-details
+   dropdown-list
+   ess
+   fuzzy
+   gnuplot-mode
+   gruvbox-theme
+   helm
+   helm-bibtex
+   helm-ls-git
+   helm-swoop
+   highlight-parentheses
+   ibuffer-git
+   ibuffer-vc
+   key-chord
+   keyfreq
+   magit 
+   multiple-cursors
+   org-gcal
+   pdf-tools
+   popup
+   region-bindings-mode
+   undo-tree
+   w3m
+   web-mode
+   yaml-mode
+   yasnippet
+   zenburn-theme
+   ))
 
 (provide 'mk_packages)
 ;;; mk_packages ends here
