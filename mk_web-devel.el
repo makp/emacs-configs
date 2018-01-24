90173ccdc3287c89bea1b452ad4d6f16fc6e1dfb
Makmiller <makmiller@gmail.com>
2018-01-23 23:33:50 -0500

    don't indent html code too much

--- mk_web-devel.el
+++ mk_web-devel.el
@@ -2,4 +2,11 @@
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
 (provide 'mk_web-devel)
