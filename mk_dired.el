d9e05b542a9c90e70cd193e2f7b434fdac923122
Makmiller <makmiller@gmail.com>
2018-01-27 20:11:41 -0500

    remove code for listing dirs first

--- mk_dired.el
+++ mk_dired.el
@@ -39,18 +39,18 @@
 	(w3m-find-file file))))
 
 ;; show directories first
-(defun mydired-sort ()
-  "Sort dired listings with directories first."
-  (save-excursion
-    (let (buffer-read-only)
-      (forward-line 2) ;; beyond dir. header 
-      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
-    (set-buffer-modified-p nil)))
+;; (defun mydired-sort ()
+;;   "Sort dired listings with directories first."
+;;   (save-excursion
+;;     (let (buffer-read-only)
+;;       (forward-line 2) ;; beyond dir. header 
+;;       (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+;;     (set-buffer-modified-p nil)))
 
-(defadvice dired-readin
-    (after dired-after-updating-hook first () activate)
-  "Sort dired listings with directories first before adding marks."
-  (mydired-sort))
+;; (defadvice dired-readin
+;;     (after dired-after-updating-hook first () activate)
+;;   "Sort dired listings with directories first before adding marks."
+;;   (mydired-sort))
 
 
 (provide 'mk_dired)
