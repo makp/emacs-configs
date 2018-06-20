1bcf863918c437a8a96935b6b9d3fdb382684a60
Makmiller <makmiller@gmail.com>
2018-06-20 16:44:11 -0400

    use package-install-selected-packages to install missing pkgs

--- mk_packages.el
+++ mk_packages.el
@@ -6,62 +6,56 @@
 
 ;;; Code:
 
-
 (require 'package)
 
+;;; Load installed packages
+
+
+;; (setq package-enable-at-startup nil)
+;; Disable automatic package loading at launch
+
+;; package-load-list
+
+(package-initialize)
+;; package-initialize searches for all the packages that are installed
+;; and loads each package's <pkg>-autoloads.el file.
+
+
+;;; Package archives
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/") t)
 
-;; check if the packages is installed; if not, install it.
-(mapc
- (lambda (package)
-   (or (package-installed-p package)
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package))))
- '(ac-math
-   ace-jump-mode
-   aggressive-indent
-   auctex
-   auto-complete
-   autopair
-   dired-details
-   dropdown-list
-   ;; ess
-   elpy
-   flycheck
-   fuzzy
-   gnuplot-mode
-   gruvbox-theme
-   helm
-   helm-bibtex
-   helm-ls-git
-   highlight-parentheses
-   ibuffer-git
-   ibuffer-vc
-   key-chord
-   keyfreq
-   magit
-   markdown-mode
-   multiple-cursors
-   nov
-   org-plus-contrib
-   org-gcal
-   pdf-tools
-   popup
-   region-bindings-mode
-   undo-tree
-   w3m
-   web-mode
-   wgrep
-   wgrep-helm
-   yaml-mode
-   yasnippet
-   zenburn-theme))
 
-(package-initialize)
+;;; Install missing packages
+
+(defvar mk/pkg-list '(ac-math ace-jump-mode aggressive-indent
+			      auctex auto-complete autopair
+			      dired-details dropdown-list elpy
+			      flycheck fuzzy gnuplot-mode
+			      gruvbox-theme helm helm-bibtex
+			      helm-ls-git highlight-parentheses
+			      ibuffer-git ibuffer-vc key-chord
+			      keyfreq magit markdown-mode
+			      multiple-cursors nov
+			      org-plus-contrib org-gcal pdf-tools
+			      popup region-bindings-mode
+			      undo-tree w3m web-mode wgrep
+			      wgrep-helm yaml-mode yasnippet
+			      zenburn-theme)
+  "The list of packages installed at launch.")
+
+;; ess
+
+;; Fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(setq package-selected-packages mk/pkg-list)
+(package-install-selected-packages)
+
 
 (provide 'mk_packages)
 
