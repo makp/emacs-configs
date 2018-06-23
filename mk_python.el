2632e4e6ab20fd706bcc209561cca4c108042cf4
Makmiller <makmiller@gmail.com>
2018-06-23 15:33:19 -0400

    add todo item; ac-mode no longer used

--- mk_python.el
+++ mk_python.el
@@ -1,9 +1,20 @@
+;;; mk_python.el --- Custom config for python mode
+
+;;; Commentary:
+
+;; TODO: add support for jupyter or ipython.
+
+;;; Code:
+
 
 ;; Use elpy-mode
 (elpy-enable)
 
 ;; elpy uses company instead of auto-complete
-(setq ac-modes (delq 'python-mode ac-modes))
+;; (setq ac-modes (delq 'python-mode ac-modes))
+
+;; when starting python shells, don't use project root as default dir
+(setq-default elpy-shell-use-project-root nil)
 
 ;; Use flycheck instead of flymake
 ;; (when (require 'flycheck nil t)
