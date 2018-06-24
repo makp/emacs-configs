a5a9998d33140c0e36317c562a8fc23e24c0e0fb
Makmiller <makmiller@gmail.com>
2018-06-23 23:24:49 -0400

    use the correct hook; organize config

--- mk_ibuffer-setup.el
+++ mk_ibuffer-setup.el
@@ -11,38 +11,15 @@
 (require 'ibuffer-git)
 
 
-(add-hook 'ibuffer-hook
-	  (lambda ()
-	    (mk/vc-refresh)
-	    (ibuffer-auto-mode 1)))	;auto-update
-
-
-(defun mk/vc-refresh ()
-  "Refresh vc-status of all buffers."
-  (interactive)
-  (dolist (b (buffer-list))
-    (with-current-buffer b
-      (vc-refresh-state))))
-
-;; ;; https://emacs.stackexchange.com/questions/35758/vc-status-behavior-in-ibuffer-vc/41024#41024
-;; (defun vc-state-refresh-post-command-hook ()
-;;   "Check if command in `this-command' was executed, then run `vc-refresh-state'"
-;;   (when (memq this-command '(other-window kill-buffer))
-;;     (vc-refresh-state)
-;;     (message "Refreshing vc-state...")))
-
-;; (add-hook 'after-save-hook 'vc-refresh-state)
-;; (add-hook 'after-revert-hook 'vc-refresh-state)
-;; (add-hook 'post-command-hook #'vc-state-refresh-post-command-hook)
-
 (setq-default
  ibuffer-show-empty-filter-groups nil ;; don't show empty filter groups
  ibuffer-expert t ;; don't ask for confirmation of "dangerous" operations.
  ibuffer-filter-group-name-face 'font-lock-variable-name-face ;;
  ibuffer-old-time 50)
 
-(dolist (ibfilter '("^\\*" "_region_"))
+(dolist (ibfilter '("^\\*" "_region_" "magit-process:.*" "magit:.*"))
   (add-to-list 'ibuffer-never-show-predicates ibfilter))
+
 
 ;; ibuffer mode maps
 (define-key ibuffer-mode-map (kbd "C-i") 'ibuffer-toggle-filter-group)
@@ -54,7 +31,43 @@
 (define-key ibuffer-mode-map (kbd "U") 'ibuffer-unmark-all)
 ;;; to be consistent with dired-mode. It was ibuffer.*regexp
 
+
+(add-hook 'ibuffer-hook
+	  (lambda ()
+	    (mk/vc-refresh)
+	    (mk/run-ibuffer-vc)))	;create filter groups based on VC
+
+(add-hook 'ibuffer-mode-hook
+	  (lambda ()
+	    (ibuffer-auto-mode 1)))	;auto-update
+
+
+(defun mk/vc-refresh ()
+  "Refresh vc-status of all buffers."
+  (interactive)
+  (dolist (b (buffer-list))
+    (with-current-buffer b
+      (vc-refresh-state))))
+
+
+;; (defun vc-state-refresh-post-command-hook ()
+;;   "Check if command in `this-command' was executed, then run `vc-refresh-state'."
+;;   (when (memq this-command '(other-window kill-buffer))
+;;     (vc-refresh-state)))
+;; (add-hook 'after-save-hook 'vc-refresh-state)
+;; (add-hook 'after-revert-hook 'vc-refresh-state)
+;; (add-hook 'post-command-hook #'vc-state-refresh-post-command-hook)
+;; https://emacs.stackexchange.com/questions/35758/vc-status-behavior-in-ibuffer-vc/41024#41024"
+
+
+(defun mk/run-ibuffer-vc ()
+  "Filter groups using ibuffer-vc."
+  (ibuffer-vc-set-filter-groups-by-vc-root)
+  (unless (eq ibuffer-sorting-mode 'alphabetic)
+    (ibuffer-do-sort-by-alphabetic)))
+
 (defun mk/open-magit-status-from-ibuffer ()
+  "Open buffer and run magit-status."
   (interactive)
   (ibuffer-visit-buffer)
   (call-interactively 'magit-status))
@@ -68,13 +81,6 @@
    ((> (buffer-size) 1000) (format "%7.3fk" (/ (buffer-size) 1000.0)))
    ((> (buffer-size) 1000000) (format "%7.3fM" (/ (buffer-size) 1000000.0)))
    (t (format "%8d" (buffer-size)))))
-
-;; ibuffer-vc
-(add-hook 'ibuffer-hook
-	  (lambda ()
-	    (ibuffer-vc-set-filter-groups-by-vc-root)
-	    (unless (eq ibuffer-sorting-mode 'alphabetic)
-	      (ibuffer-do-sort-by-alphabetic))))
 
 ;; Type '`' to switch through different ibuffer-formats. Use "," to
 ;; change how files are sorted.
