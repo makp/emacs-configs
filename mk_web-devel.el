a1ac0b72175000ddd360a2ff3fe47ef07ad79a0f
Makmiller <makmiller@gmail.com>
2018-08-11 16:30:55 -0400

    increase CSS indentation

--- mk_web-devel.el
+++ mk_web-devel.el
@@ -7,9 +7,9 @@
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2) ;HTML
+  (setq web-mode-css-indent-offset 3)	 ;CSS
+  (setq web-mode-code-indent-offset 2))  ;scriptcode (e.g., JavaScript, PHP, etc)
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
