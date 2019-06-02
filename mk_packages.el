0aaefc3574f16937e35b2dfe5e941034e397e4c9
mak-leibniz <makmiller@gmail.com>
2019-06-02 12:23:17 -0400

    fix name pkg

--- mk_packages.el
+++ mk_packages.el
@@ -42,7 +42,7 @@
 			  org-plus-contrib org-gcal org-ref
 			  pdf-tools region-bindings-mode
 			  undo-tree w3m web-mode wgrep wgrep-helm
-			  yaml-mode yasnippet wolfram)
+			  yaml-mode yasnippet wolfram-mode)
   "The list of packages installed at launch.")
 
 ;; ess, ibuffer-git
