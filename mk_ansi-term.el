257c04cd244e7c97091d21d579366863f0b5fb3b
mak-turing <makmiller@gmail.com>
2019-06-29 23:29:27 -0400

    use zsh instead of fish

--- mk_ansi-term.el
+++ mk_ansi-term.el
@@ -6,7 +6,7 @@
   "Toggle an ansi-term buffer."
   (interactive)
   (when (not (get-buffer "*ansi-term*"))
-    (save-window-excursion (ansi-term "/usr/bin/fish")))
+    (save-window-excursion (ansi-term "/usr/bin/zsh")))
   (if (equal (buffer-name) "*ansi-term*")
       (quit-window)
     (switch-to-buffer-other-window "*ansi-term*")))
@@ -17,7 +17,7 @@
   (interactive "P")
   (if (not (consp arg))
       (mk/ansi-term-popup)
-    (ansi-term "/usr/bin/fish")))
+    (ansi-term "/usr/bin/zsh")))
 
 (setq term-buffer-maximum-size 0)
 ;;; 
