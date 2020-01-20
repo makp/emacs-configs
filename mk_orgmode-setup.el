6637494f522085b6e4bbbd69db38e1a27158c3e5
mak-turing <makmiller@gmail.com>
2020-01-20 13:40:54 -0500

    minimize gcal alerts

--- mk_orgmode-setup.el
+++ mk_orgmode-setup.el
@@ -322,6 +322,8 @@
 (add-hook 'org-agenda-mode-hook (lambda () (org-gcal-sync)))
 ;; (add-hook 'org-capture-after-finalize-hook (lambda () (org-gcal-sync)))
 
+(setq org-gcal-notify-p nil)
+
 
 ;; =============
 ;; inline images
