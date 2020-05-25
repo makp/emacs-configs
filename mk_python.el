7f5c6d1b82bcfa392ce8817f7dc2349ba943ff18
mak-turing <makmiller@gmail.com>
2020-05-24 21:22:55 -0400

    del unnecessary code

--- mk_python.el
+++ mk_python.el
@@ -13,10 +13,5 @@
 ;; when starting python shells, don't use project root as default dir
 (setq elpy-shell-use-project-root nil)
 
-;; Use flycheck instead of flymake
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
 (provide 'mk_python)
 ;;; mk_python.el ends here
