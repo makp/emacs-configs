f94093ecc84a9dd7f2728bbed25b81900be91a79
mak-turing <makmiller@gmail.com>
2020-05-25 00:01:57 -0400

    clean config for pkgs

--- mk_packages.el
+++ mk_packages.el
@@ -8,18 +8,10 @@
 
 (require 'package)
 
-;;; Load installed packages
-
-
 ;; (setq package-enable-at-startup nil)
 ;; Disable automatic package loading at launch
 
 ;; package-load-list
-
-(package-initialize)
-;; package-initialize searches for all the packages that are installed
-;; and loads each package's <pkg>-autoloads.el file.
-
 
 ;;; Package archives
 (add-to-list 'package-archives
@@ -29,31 +21,70 @@
 	     '("org" . "http://orgmode.org/elpa/") t)
 
 
-;;; Install missing packages
+(package-initialize)
+;; package-initialize searches for all the packages that are installed
+;; and loads each package's <pkg>-autoloads.el file.
 
-(defvar mk/pkg-list '(avy aggressive-indent auctex company
-			  company-auctex company-math define-word elpy
-			  flycheck fuzzy gnuplot-mode
-			  gruvbox-theme helm helm-bibtex
-			  helm-ls-git highlight-parentheses
-			  hl-todo ibuffer-vc ibuffer-git
-			  key-chord keyfreq lua-mode magit
-			  markdown-mode multiple-cursors nov org
-			  org-plus-contrib org-gcal org-ref
-			  pdf-tools poporg region-bindings-mode synosaurus
-			  undo-tree w3m visual-fill-column
-			  web-mode wgrep wgrep-helm yaml-mode
-			  yasnippet wolfram-mode)
-  "The list of packages installed at launch.")
-
-;; ess, ibuffer-git
 
 ;; Fetch the list of packages available
 (unless package-archive-contents
   (package-refresh-contents))
 
-(setq package-selected-packages mk/pkg-list)
-(package-install-selected-packages)
+
+;; Scans the list in mk/pkg-list. If the package listed is not already installed, install it
+(defvar mk/pkg-list '(avy
+		      aggressive-indent
+		      auctex company
+		      company-auctex
+		      company-math
+		      define-word
+		      elpy
+		      ;; ess
+		      flycheck
+		      fuzzy
+		      gnuplot-mode
+		      gruvbox-theme
+		      helm
+		      helm-bibtex
+		      helm-ls-git
+		      highlight-parentheses
+		      hl-todo
+		      ibuffer-vc
+		      ibuffer-git
+		      key-chord
+		      keyfreq
+		      lua-mode
+		      magit
+		      markdown-mode
+		      multiple-cursors
+		      nov
+		      org
+		      org-plus-contrib
+		      org-gcal
+		      org-ref
+		      pdf-tools
+		      poporg
+		      region-bindings-mode
+		      synosaurus
+		      undo-tree
+		      w3m
+		      visual-fill-column
+		      web-mode
+		      wgrep
+		      wgrep-helm
+		      yaml-mode
+		      yasnippet
+		      wolfram-mode)
+  "The list of packages installed at launch.")
+
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      mk/pkg-list)
+
+;; Older code:
+;; (setq package-selected-packages mk/pkg-list)
+;; (package-install-selected-packages)
 
 
 (provide 'mk_packages)
