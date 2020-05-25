732f9ae53be746a03556427bc5cd4c3083321ca7
mak-turing <makmiller@gmail.com>
2020-05-25 00:01:39 -0400

    use jupyter console instead

--- mk_python.el
+++ mk_python.el
@@ -2,7 +2,7 @@
 
 ;;; Commentary:
 
-;; TODO: add support for jupyter or ipython.
+;; TODO:
 
 ;;; Code:
 
@@ -11,7 +11,13 @@
 (elpy-enable)
 
 ;; when starting python shells, don't use project root as default dir
-(setq elpy-shell-use-project-root nil)
+;; (setq elpy-shell-use-project-root nil)
+
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
 
 (provide 'mk_python)
 ;;; mk_python.el ends here
