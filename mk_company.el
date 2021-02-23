6867612e48bff7c9837983bdce820fd22d398546
mak-turing <makmiller@gmail.com>
2021-02-23 10:58:00 -0500

    Comment old company customization; Specify modes company is loaded

--- mk_company.el
+++ mk_company.el
@@ -6,17 +6,17 @@
 
 ;;; Code:
 
-;; (add-hook 'after-init-hook 'global-company-mode)
+(setq-default company-global-modes '(text-mode emacs-lisp-mode LaTeX-mode python-mode))
 (global-company-mode)
 
-(setq-default company-idle-delay 0.25)
-(setq-default company-minimum-prefix-length 3)
+;; (setq-default company-idle-delay 0.25)
+;; (setq-default company-minimum-prefix-length 3)
 
-(global-set-key (kbd "<tab>") 'company-complete-common-or-cycle) ;TAB
-(define-key company-active-map (kbd "TAB") 'company-complete) ;C-i
-(define-key company-active-map (kbd "C-s") 'nil)
-(define-key company-active-map (kbd "M-s") 'company-search-candidates)
-(define-key company-active-map (kbd "C-m") nil)
+;; (global-set-key (kbd "<tab>") 'company-complete-common-or-cycle) ;TAB
+;; (define-key company-active-map (kbd "TAB") 'company-complete) ;C-i
+;; (define-key company-active-map (kbd "C-s") 'nil)
+;; (define-key company-active-map (kbd "M-s") 'company-search-candidates)
+;; (define-key company-active-map (kbd "C-m") nil)
 
 
 ;; ========
@@ -27,28 +27,28 @@
 ;; Backend for LaTeX files
 ;; -----------------------
 
-(defun mk/company-tex-backend ()
-  "Add company backends provided by company-math for LaTeX math symbols."
-  (add-to-list 'company-backends '(company-math-symbols-latex
-				   company-math-symbols-unicode
-				   company-latex-commands)))
+;; (defun mk/company-tex-backend ()
+;;   "Add company backends provided by company-math for LaTeX math symbols."
+;;   (add-to-list 'company-backends '(company-math-symbols-latex
+;; 				   company-math-symbols-unicode
+;; 				   company-latex-commands)))
 
-(with-eval-after-load 'tex-mode
-  (mk/company-tex-backend)
-  (company-auctex-init)) ;company-auctex
+;; (with-eval-after-load 'tex-mode
+;;   (mk/company-tex-backend)
+;;   (company-auctex-init)) ;company-auctex
 
-;; Add yasnippet support for all company backends
-;; https://github.com/syl20bnr/spacemacs/pull/179
-(defvar company-mode/enable-yas t
-  "Enable yasnippet for all backends.")
+;; ;; Add yasnippet support for all company backends
+;; ;; https://github.com/syl20bnr/spacemacs/pull/179
+;; (defvar company-mode/enable-yas t
+;;   "Enable yasnippet for all backends.")
 
-(defun company-mode/backend-with-yas (backend)
-  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-      backend
-    (append (if (consp backend) backend (list backend))
-            '(:with company-yasnippet))))
+;; (defun company-mode/backend-with-yas (backend)
+;;   (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+;;       backend
+;;     (append (if (consp backend) backend (list backend))
+;;             '(:with company-yasnippet))))
 
-(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+;; (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
 (provide 'mk_company)
 
