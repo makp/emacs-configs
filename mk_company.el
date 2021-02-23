6f3f147d5f225f87937c1108b0528da470ea5544
mak-turing <makmiller@gmail.com>
2021-02-23 12:36:00 -0500

    Uncomment code for adding yasnippet to all backends

--- mk_company.el
+++ mk_company.el
@@ -22,10 +22,22 @@
 ;; ========
 ;; Backends
 ;; ========
+;; Var: `company-backends'
 
-;; -----------------------
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
+
+(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+
 ;; Backend for LaTeX files
-;; -----------------------
 
 ;; (defun mk/company-tex-backend ()
 ;;   "Add company backends provided by company-math for LaTeX math symbols."
@@ -36,19 +48,6 @@
 ;; (with-eval-after-load 'tex-mode
 ;;   (mk/company-tex-backend)
 ;;   (company-auctex-init)) ;company-auctex
-
-;; ;; Add yasnippet support for all company backends
-;; ;; https://github.com/syl20bnr/spacemacs/pull/179
-;; (defvar company-mode/enable-yas t
-;;   "Enable yasnippet for all backends.")
-
-;; (defun company-mode/backend-with-yas (backend)
-;;   (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-;;       backend
-;;     (append (if (consp backend) backend (list backend))
-;;             '(:with company-yasnippet))))
-
-;; (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
 (provide 'mk_company)
 
