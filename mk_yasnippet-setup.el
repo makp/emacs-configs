41717ab1089d2c247a0375a45611fc62b4312026
mak-turing <makmiller@gmail.com>
2021-03-10 14:52:44 -0500

    Del buggy function

--- mk_yasnippet-setup.el
+++ mk_yasnippet-setup.el
@@ -35,27 +35,7 @@
 (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
 ;; supress warning when using backquote expansions
 
-(setq-default yas-prompt-functions '(shk-yas/helm-prompt yas-dropdown-prompt))
-
-(defun shk-yas/helm-prompt (prompt choices &optional display-fn)
-  "Use helm to select a snippet."
-  (interactive)
-  (setq display-fn (or display-fn 'identity))
-  (if (require 'helm-config)
-      (let (tmpsource cands result rmap)
-	(setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
-	(setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
-	(setq tmpsource
-	      (list
-	       (cons 'name prompt)
-	       (cons 'candidates cands)
-	       '(action . (("Expand" . (lambda (selection) selection))))
-	       ))
-	(setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
-	(if (null result)
-	    (signal 'quit "user quit!")
-	  (cdr (assoc result rmap))))
-    nil))
+;; yas-prompt-functions
 
 ;; --------------------
 ;; interaction with yas
