a02c3ce45c77332b223367c1f869c23e37c51870
mak-leibniz <makmiller@gmail.com>
2021-03-13 00:12:49 -0500

    Swap lines; Del unneeded code

--- mk_yasnippet-setup.el
+++ mk_yasnippet-setup.el
@@ -18,11 +18,11 @@
 (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
 
 
-;; yas-trigger-key
-;; The key bound to `yas-expand' when `yas-minor-mode' is active.
-
 ;; yas-wrap-around-region
 ;; If non-nil, snippet expansion wraps around selected region for $0 field.
+
+;; yas-trigger-key
+;; The key bound to `yas-expand' when `yas-minor-mode' is active.
 
 ;; yas-indent-line 'fixed
 ;; This variable controls indenting. The default value, "auto",
@@ -33,24 +33,6 @@
 ;; Var controls whether to add a new line at the end of the file
 
 ;; yas-prompt-functions
-
-;; --------------------
-;; interaction with yas
-;; --------------------
-;; (add-hook 'org-mode-hook
-;; 	  (lambda ()
-;; 	    (org-set-local 'yas-trigger-key [tab])
-;; 	    (define-key yas-keymap [tab] 'yas-next-field-group)))
-
-;; (defun yas-org-very-safe-expand ()
-;;   (let ((yas-fallback-behavior 'return-nil)) (yas-expand)))
-
-;; (add-hook 'org-mode-hook
-;; 	  (lambda ()
-;; 	    (make-variable-buffer-local 'yas-trigger-key)
-;; 	    (setq yas-trigger-key [tab])
-;; 	    (add-to-list 'org-tab-first-hook 'yas-org-very-safe-expand)
-;; 	    (define-key yas-keymap [tab] 'yas-next-field)))
 
 ;;; Inter-field navigation
 ;; (defun yas-goto-end-of-active-field ()
