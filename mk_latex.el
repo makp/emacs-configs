d720932acfa5cc9c8444482bf558ee8d0dfb62b6
mak-leibniz <makmiller@gmail.com>
2021-03-19 23:17:00 -0400

    Del code

--- mk_latex.el
+++ mk_latex.el
@@ -287,25 +287,6 @@
 	("(" (lambda ()(interactive)(mk/LaTeX-leftright "\\left( " "\\right)")) "" nil))) ;it was langle originally
 
 
-;; LaTeX-math-abbrev-prefix wraps $$ around symbol when in text mode
-;; (from StackExchange TeX)
-;; (add-hook
-;;  'LaTeX-mode-hook
-;;  (lambda ()
-;;    (let ((math (reverse (append LaTeX-math-list LaTeX-math-default))))
-;;      (while math
-;;        (let ((entry (car math))
-;; 	     value)
-;; 	 (setq math (cdr math))
-;; 	 (if (listp (cdr entry))
-;; 	     (setq value (nth 1 entry))
-;; 	   (setq value (cdr entry)))
-;; 	 (if (stringp value)
-;; 	     (fset (intern (concat "LaTeX-math-" value))
-;; 		   (list 'lambda (list 'arg) (list 'interactive "*P")
-;; 			 (list 'LaTeX-math-insert value
-;; 			       '(null (texmathp)))))))))))
-
 ;; -------
 ;; Preview
 ;; -------
