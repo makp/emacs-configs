1a60fb8ed4af32884d6c4308a63a51735e406f21
mak-turing <makmiller@gmail.com>
2023-09-07 12:16:20 -0400

    Add func to select GPT model

--- mk_chatgpt.el
+++ mk_chatgpt.el
@@ -1,9 +1,14 @@
 (message "Carregando mk_chatgpt")
 
 (evil-set-initial-state 'chatgpt-mode 'emacs)
+(setq openai-key (getenv "OPENAI_KEY"))
 
-(setq chatgpt-model "gpt-4" ;; "gpt-3.5-turbo"
-      openai-key (getenv "OPENAI_KEY"))
+(defun mk/chatgpt ()
+  "Select GPT model to use."
+  (interactive)
+  (let ((gpt-version (completing-read "O que voce quer?: " '("gpt-4" "gpt-3.5-turbo"))))
+    (setq chatgpt-model gpt-version))
+  (chatgpt))
 
 
 (provide 'mk_chatgpt)
