5e9fa8e419e10b463d5a51fbb7d319ace5854ecb
mak-turing <makmiller@gmail.com>
2023-09-08 15:07:57 -0400

    Change keybindings

--- mk_copilot.el
+++ mk_copilot.el
@@ -5,10 +5,14 @@
 
 (add-hook 'prog-mode-hook 'copilot-mode)
 
-(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
-(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
 
-;; copilot-accept-completion-by-word
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion-by-word)
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "C-<tab>") 'copilot-next-completion) ;; copilot-previous-completion
+
+;; I don't think this line is necessary
+;; (customize-set-variable 'copilot-enable-predicates '(evil-insert-state-p))
+
 
 (provide 'mk_copilot)
 ;;; mk_copilot.el ends here
