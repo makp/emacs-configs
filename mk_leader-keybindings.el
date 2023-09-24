c1238a281eb9d1745df1f5bd6c481e7eae0f982a
mak-turing <makmiller@gmail.com>
2023-09-23 23:44:19 -0400

    Add keybinding to `magit-repolist`

--- mk_leader-keybindings.el
+++ mk_leader-keybindings.el
@@ -48,6 +48,7 @@
   (kbd "<leader>gb") 'helm-browse-project ; accepts prefix arg
 
   ;; project
+  (kbd "<leader>pp") 'magit-repolist
   (kbd "<leader>ps") 'mk/select-project-and-display-status
   (kbd "<leader>ph") 'helm-projects-history
   (kbd "<leader>pg") 'mk/select-project-and-run-git-grep
