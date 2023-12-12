de23613623e47defab3965526b935a4487ed52f6
mak-turing <makmiller@gmail.com>
2023-12-12 11:36:06 -0500

    Del older GPT models from the list of options

--- mk_chatgpt.el
+++ mk_chatgpt.el
@@ -23,7 +23,7 @@
 (defun mk/chatgpt-select-model ()
   "Select GPT model to use."
   (interactive)
-  (let ((gpt-version (completing-read "Que ce quer?: " '("gpt-4" "gpt-3.5-turbo" "gpt-4-1106-preview" "gpt-3.5-turbo-1106"))))
+  (let ((gpt-version (completing-read "Que ce quer?: " '("gpt-4-1106-preview" "gpt-3.5-turbo-1106"))))
     (setq chatgpt-model gpt-version))
   (chatgpt)
   (delete-other-windows)
