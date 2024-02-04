946f411ab4ece01652b7bd2b3b9b09d32b68e6cc
mak-turing <makmiller@gmail.com>
2024-02-04 18:46:03 -0500

    Update GPT models

--- mk_chatgpt.el
+++ mk_chatgpt.el
@@ -23,7 +23,7 @@
 (defun mk/chatgpt-select-model ()
   "Select GPT model to use."
   (interactive)
-  (let ((gpt-version (completing-read "Que ce quer?: " '("gpt-4-1106-preview" "gpt-3.5-turbo-1106"))))
+  (let ((gpt-version (completing-read "Que ce quer?: " '("gpt-4-0125-preview" "gpt-3.5-turbo-0125"))))
     (setq chatgpt-model gpt-version))
   (chatgpt)
   (delete-other-windows)
